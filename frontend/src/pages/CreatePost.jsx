abad6ee8f7146282b8b4a3b4fae1f11a6477eb03
Narinder Maurya <narinder06@pm.me>
2023-03-13 23:46:51 +0530

    Final

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -61,7 +61,7 @@ const CreatePost = () => {
       setLoading(true);
       try {
         const response = await fetch(
-          "http://localhost:8080/api/v1/post",
+          "https://imageforge-nbmn.onrender.com/api/v1/post",
           {
             method: "POST",
             headers: {
